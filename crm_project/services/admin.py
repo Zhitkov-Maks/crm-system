8498afc6c6dae75cb815dabfccb15bc926a520fb
Maksim <m-zhitkov@inbox.ru>
2024-03-03 11:14:29 +0300

    Configured the operation of the products

--- crm_project/services/admin.py
+++ crm_project/services/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Products
 
-# Register your models here.
+
+@admin.register(Products)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'cost')
+    list_display_links = ('name',)
--- crm_project/services/models.py
+++ crm_project/services/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    """Модель для представления услуг."""
+    name = models.CharField(max_length=200, verbose_name='Услуга')
+    description = models.TextField(max_length=10_000, verbose_name='Описание')
+    cost = models.DecimalField(verbose_name="Цена", max_digits=14, decimal_places=2)
+
+    def __str__(self):
+        return f'{str(self.name)[:40]}...'
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'

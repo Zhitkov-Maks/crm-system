aa61f568b7c25b99bf4d20c8f71084fe19dd0330
Maksim <m-zhitkov@inbox.ru>
2024-03-16 08:31:25 +0300

    Added applications to display the main page

--- crm_project/app/urls.py
+++ crm_project/app/urls.py
@@ -2,6 +2,4 @@ from django.urls import path
 from .views import IndexView
 
 
-urlpatterns = [
-    path('', IndexView.as_view(), name='index')
-]
+urlpatterns = [path("", IndexView.as_view(), name="index")]

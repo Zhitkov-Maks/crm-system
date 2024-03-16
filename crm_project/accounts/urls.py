4e6cb55eb6d8211d015ac03da5bf78dfcf19abfe
Maksim <m-zhitkov@inbox.ru>
2024-03-16 13:03:17 +0300

    Added annotations and documentation

--- crm_project/accounts/urls.py
+++ crm_project/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import MyLoginView, MyLogoutView
 
 
-urlpatterns = [
+urlpatterns: list = [
     path("login/", MyLoginView.as_view(), name="login"),
     path("logout/", MyLogoutView.as_view(), name="logout"),
 ]

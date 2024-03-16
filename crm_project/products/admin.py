b738b911dc94db5cf7ab8b1c24f942f974d8ce84
Maksim <m-zhitkov@inbox.ru>
2024-03-16 13:45:38 +0300

    Edit admin.py

--- crm_project/products/admin.py
+++ crm_project/products/admin.py
@@ -4,6 +4,7 @@ from .models import Products
 
 @admin.register(Products)
 class ProductAdmin(admin.ModelAdmin):
+    """Настройки админки для услуг."""
     list_display = ("pk", "name", "cost")
     list_display_links = ("name",)
     search_fields = ("name",)

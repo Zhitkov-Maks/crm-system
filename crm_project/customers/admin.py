a1184f613f4e33e74711859b491c612db7f3ec60
Maksim <m-zhitkov@inbox.ru>
2024-03-16 13:39:35 +0300

    Edited admin

--- crm_project/customers/admin.py
+++ crm_project/customers/admin.py
@@ -6,5 +6,5 @@ from .models import Customers
 class CustomerAdmin(admin.ModelAdmin):
     """Настройки админки для Активных клиентов."""
     list_display = ("id", "lead", "contract")
-    autocomplete_fields = ("lead",)
+    autocomplete_fields = ("lead", "contract")
     list_display_links = ("id", "lead")
